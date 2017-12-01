b0077d819aeae8455716316a7b0ccf62204efef7
Nicole Stein <nicolethenerd@gmail.com>
2017-11-30 20:27:14 -0500

    Add card flip

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// When #flip-btn is clicked
+$('#flip-btn').click(function() {
+  // Toggle the .flipped class on the card
+  $('.card').toggleClass('flipped');
+});
+
+// $('.container').hover(function() {
+//   // Toggle the .flipped class on the card
+//   $('.card').toggleClass('flipped');
+// });
